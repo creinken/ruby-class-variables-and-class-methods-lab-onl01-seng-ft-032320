8cb3c7b07f68785b844213ef062be1ee800686e0
Christopher Reinken <christopherreinken@gmail.com>
2020-03-31 17:18:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,4 +17,19 @@ class Song
     @@count
   end
   
+  def self.artists
+    
+  end
+  
+  def self.genres
+    
+  end
+  
+  def self.genre_count
+    
+  end
+  
+  def self.artist_count
+    
+  end
 end
